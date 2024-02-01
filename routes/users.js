b6e531ac2e63868f88b13d0af1042925a21c8b36
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-01 16:44:33 -0500

    Add routes for CRUD operations on user resource

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/", (req, res, next) => {
+  return res.send("GET HTTP method on user resource");
+});
+
+router.post("/", (req, res, next) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+router.put("/", (req, res, next) => {
+  return res.send("PUT HTTP method on user resource");
+});
+
+router.delete("/", (req, res, next) => {
+  return res.send("DELETE HTTP method on user resource");
 });
 
 module.exports = router;
