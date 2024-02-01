069037d11af028a7c63b2f9effe2f0264c136584
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2024-02-01 16:38:04 -0500

    Add routes responding to different http methods

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+router.get("/", (req, res, next) => {
+  return res.send("Recieved a GET HTTP method");
+});
+
+router.post("/", (req, res, next) => {
+  return res.send("Recieved a POST HTTP method");
+});
+
+router.put("/", (req, res, next) => {
+  return res.send("Recieved a PUT HTTP method");
+});
+
+router.delete("/", (req, res, next) => {
+  return res.send("Recieved a DELETE HTTP method");
 });
 
 module.exports = router;
